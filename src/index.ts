84f47a0709275d5d06fe225c5fa27c847c6033dd
yanguoyu_cd <yanguoyu_cd@keruyun.com>
2018-03-08 10:08:26 +0800

    1：搭建ts环境。

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
 import hello from './hello'
-export default hello
+import { connect, launch } from 'puppeteer';
+
+function test1(){
+  (async () => {
+    const browser = await launch();
+    const page = await browser.newPage();
+    await page.goto('https://example.com');
+    await page.screenshot({path: 'example.png'});
+  
+    await browser.close();
+  })();
+}
+
+export {
+  hello,
+  test1
+}
