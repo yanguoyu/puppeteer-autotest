cac42c644087777da61a2ff80b435a351fbe5202
yanguoyu_cd <yanguoyu_cd@keruyun.com>
2018-04-24 15:08:30 +0800

    1：修改导出方式。

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import ElementAnalysis from './elementAnalysis';
 import PuppeteerTool from './puppeteerTool';
-import PuppeteerModels from './model/index';
+import AttributeTypes from './model/attributeTypes.model'
+import EleEventTypes from './model/eleEventTypes.model'
+import ElementModel from './model/element.model'
+import EleOperatorModel from './model/elementOperator.model'
+import ElementSelectKey from './model/elementSelectKey.model'
+import ElementTypes from './model/elementTypes.model'
 
 export {
   PuppeteerTool,
   ElementAnalysis,
-  PuppeteerModels
+  AttributeTypes,
+  EleEventTypes,
+  ElementModel,
+  EleOperatorModel,
+  ElementSelectKey,
+  ElementTypes
 }
